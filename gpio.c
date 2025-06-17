4218183dace3e698b14550b3ba28f14b6fc9c69a
Hashu-17 <neutrinohash2@gmail.com>
2025-06-17 09:20:00 +0530

    add Makefile skeleton

--- gpio.c
+++ gpio.c
@@ -1,22 +1,23 @@
 #include "gpio.h"
 
 void gpio_init(gpio_config_t *cfg) {
+    if (cfg == NULL) {
+        return;
+    }
     if (cfg->port == GPIOA) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     else if (cfg->port == GPIOB) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
     else if (cfg->port == GPIOC) RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-
-    cfg->port->MODER &= ~(0x3 << (cfg->pin * 2));
-    cfg->port->MODER |= (cfg->mode << (cfg->pin * 2));
+    cfg->port->MODER &= ~(0x3U << (cfg->pin * 2U));
+    cfg->port->MODER |= ((uint32_t)cfg->mode << (cfg->pin * 2U));
 }
 
 void gpio_write(GPIO_TypeDef *port, uint8_t pin, bool value) {
     if (value)
-        port->ODR |= (1 << pin);
+        port->ODR |= (1U << pin);
     else
-        port->ODR &= ~(1 << pin);
+        port->ODR &= ~(1U << pin);
 }
 
 bool gpio_read(GPIO_TypeDef *port, uint8_t pin) {
-    return (port->IDR >> pin) & 0x1;
+    return ((port->IDR >> pin) & 0x1U) != 0U;
 }
-//new version of gpio.c

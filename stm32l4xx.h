1bdd3914dbba43f37f9a5ba4ddf767396eedef00
Hashu-17 <neutrinohash2@gmail.com>
2025-07-09 19:09:00 +0530

    add test_gpio_speed placeholder

--- stm32l4xx.h
+++ stm32l4xx.h
@@ -1,7 +1,15 @@
 #ifndef STM32L4XX_H
 #define STM32L4XX_H
 
-typedef struct GPIO_TypeDef GPIO_TypeDef;
-typedef struct RCC_TypeDef RCC_TypeDef;
+#include <stdint.h>
+
+typedef struct {
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDR;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+} GPIO_TypeDef;
 
 #endif

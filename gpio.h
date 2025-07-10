6969cbac1036da88563aa6a4d0f927917ddde146
Hashu-17 <neutrinohash2@gmail.com>
2025-07-10 11:35:00 +0530

    add GPIO_PIN_COUNT macro

--- gpio.h
+++ gpio.h
@@ -1,10 +1,11 @@
-#ifndef GPIO_H
-#define GPIO_H
+#ifndef GPIO_DRIVER_H
+#define GPIO_DRIVER_H
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <stddef.h>
 #include "stm32l4xx.h"
+
+#define GPIO_PIN_COUNT 16U
 
 typedef enum {
     GPIO_MODE_INPUT = 0x00,
@@ -12,6 +13,17 @@ typedef enum {
     GPIO_MODE_ALT = 0x02,
     GPIO_MODE_ANALOG = 0x03
 } gpio_mode_t;
+
+typedef enum {
+    GPIO_OUTPUT_PUSHPULL = 0x00,
+    GPIO_OUTPUT_OPENDRAIN = 0x01
+} gpio_output_type_t;
+
+typedef enum {
+    GPIO_PULL_NONE = 0x00,
+    GPIO_PULL_UP = 0x01,
+    GPIO_PULL_DOWN = 0x02
+} gpio_pull_t;
 
 typedef enum {
     GPIO_SPEED_LOW = 0x00,
@@ -29,11 +41,20 @@ typedef struct {
     GPIO_TypeDef *port;
     uint8_t pin;
     gpio_mode_t mode;
+    gpio_output_type_t output_type;
+    gpio_pull_t pull;
     gpio_speed_t speed;
+    uint8_t alternate;
 } gpio_config_t;
 
-void gpio_init(const gpio_config_t *cfg);
+void gpio_port_enable(GPIO_TypeDef *port);
+void gpio_init(gpio_config_t *cfg);
+void gpio_pin_init(gpio_pin_t *pin, gpio_mode_t mode);
 void gpio_write(GPIO_TypeDef *port, uint8_t pin, bool value);
 bool gpio_read(GPIO_TypeDef *port, uint8_t pin);
+void gpio_toggle(GPIO_TypeDef *port, uint8_t pin);
+void gpio_pin_write(gpio_pin_t *pin, bool value);
+bool gpio_pin_read(gpio_pin_t *pin);
+void gpio_pin_toggle(gpio_pin_t *pin);
 
 #endif
--- tests/test_registry.c
+++
@@ -1,6 +0,0 @@
-#include <stddef.h>
-
-const char *gpio_tests[] = {
-    "test_gpio_speed",
-};
-const size_t gpio_test_count = sizeof(gpio_tests) / sizeof(gpio_tests[0]);

8df108fbdcc21c4e31c4280a8f8d5c0aa4aa51dd
Hashu-17 <neutrinohash2@gmail.com>
2025-07-10 09:05:00 +0530

    add test_gpio_pull placeholder

---
+++ scripts/run_tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+make -C tests clean all
--- tests/test_gpio_speed.c
+++
@@ -1,9 +0,0 @@
-#include "gpio.h"
-#include "test_helpers.h"
-
-int main(void) {
-    gpio_speed_t speed = GPIO_SPEED_LOW;
-    (void)speed;
-    TEST_ASSERT(GPIO_SPEED_LOW == 0);
-    return 0;
-}

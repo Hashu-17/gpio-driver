b6f7f54589db055ff46208dafca338e0fadd00c3
Hashu-17 <neutrinohash2@gmail.com>
2025-07-11 18:43:00 +0530

    add minimal stm32l4xx register stubs

--- stm32l4xx.h
+++ stm32l4xx.h
@@ -1,7 +1,37 @@
 #ifndef STM32L4XX_H
 #define STM32L4XX_H
 
-typedef struct GPIO_TypeDef GPIO_TypeDef;
-typedef struct RCC_TypeDef RCC_TypeDef;
+#include <stdint.h>
+
+typedef struct {
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+} GPIO_TypeDef;
+
+typedef struct {
+    volatile uint32_t AHB1ENR;
+} RCC_TypeDef;
+
+#define PERIPH_BASE 0x40000000U
+#define AHB1PERIPH_BASE (PERIPH_BASE + 0x00020000U)
+
+#define GPIOA_BASE (AHB1PERIPH_BASE + 0x0000U)
+#define GPIOB_BASE (AHB1PERIPH_BASE + 0x0400U)
+#define GPIOC_BASE (AHB1PERIPH_BASE + 0x0800U)
+
+#define RCC_BASE (PERIPH_BASE + 0x00021000U)
+
+#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE)
+#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE)
+#define GPIOC ((GPIO_TypeDef *)GPIOC_BASE)
+#define RCC   ((RCC_TypeDef *)RCC_BASE)
+
+#define RCC_AHB1ENR_GPIOAEN (1U << 0)
+#define RCC_AHB1ENR_GPIOBEN (1U << 1)
+#define RCC_AHB1ENR_GPIOCEN (1U << 2)
 
 #endif

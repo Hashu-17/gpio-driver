6ff9567243c986649d2c462ca844fb6ebad59e08
Hashu-17 <neutrinohash2@gmail.com>
2025-07-15 21:15:00 +0530

    add pull-up/down configuration

--- gpio.h
+++ gpio.h
@@ -54,6 +54,7 @@ void gpio_init(gpio_config_t *cfg);
 void gpio_pin_init(gpio_pin_t *pin, gpio_mode_t mode);
 void gpio_set_mode(GPIO_TypeDef *port, uint8_t pin, gpio_mode_t mode);
 void gpio_set_output_type(GPIO_TypeDef *port, uint8_t pin, gpio_output_type_t otype);
+void gpio_set_pull(GPIO_TypeDef *port, uint8_t pin, gpio_pull_t pull);
 void gpio_set_speed(GPIO_TypeDef *port, uint8_t pin, gpio_speed_t speed);
 void gpio_write(GPIO_TypeDef *port, uint8_t pin, bool value);
 bool gpio_read(GPIO_TypeDef *port, uint8_t pin);

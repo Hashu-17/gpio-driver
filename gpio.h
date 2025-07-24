f7844eab846f90de1f65f583260a9a000ced24e4
Hashu-17 <neutrinohash2@gmail.com>
2025-07-24 12:00:00 +0530

    add inline helpers in gpio.h

--- gpio.h
+++ gpio.h
@@ -80,4 +80,9 @@ void gpio_pin_write_mask(GPIO_TypeDef *port, uint16_t set_mask, uint16_t reset_m
 void gpio_port_apply(const gpio_port_config_t *cfg);
 gpio_config_t gpio_pin_config_defaults(gpio_pin_t pin);
 
+static inline gpio_pin_t gpio_pin(GPIO_TypeDef *port, uint8_t pin) {
+    gpio_pin_t out = { port, pin };
+    return out;
+}
+
 #endif
